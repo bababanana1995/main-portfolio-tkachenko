0f6c8684c452f04b70be572c941ec8ed623ad873
Danil <baba_ejik@mail.ru>
2023-07-19 18:36:17 +0600

    1 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Header} from "./component/header/Header";
+import {BrowserRouter, Route,Routes} from "react-router-dom";
+import {WelcomeBlock} from "./component/welcome_block/WelkomeBlock";
+import {Skills} from "./component/skils/Skils";
+import {Contact} from "./component/contact/Contact";
+import {MyProjects} from "./component/my_works/MyProjects";
+import {Footer} from "./component/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <div className="App">
+              <Header/>
+              <Routes>
+                  <Route path={'/welcome'} element={<WelcomeBlock/>}/>
+                  <Route path={'/skills'} element={<Skills/>}/>
+                  <Route path={'/my_projects'} element={<MyProjects/>}/>
+                  <Route path={'contacts'} element={<Contact/>}/></Routes>
+                  <Footer/>
+          </div>
+      </BrowserRouter>
   );
 }
 
